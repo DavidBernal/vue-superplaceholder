9779e36bc465034a116d468dc16be49a27303993
David <dbl1987@gmail.com>
2016-04-11 22:42:31 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # vue-superplaceholder
+
+Vue directive based on [superplaceholder.js](https://github.com/chinchang/superplaceholder.js)
+
+
+## Usage
+
+String mode:
+
+     <input type="text"
+     placeholder="username"
+     v-superplaceholder="'Put your user'">
+
+
+Array mode:
+
+     <input type="text"
+     placeholder="username"
+     v-superplaceholder="['Put your user','Be original']">
---
+++ superplaceholder.js
@@ -0,0 +1,40 @@
+var Vue = require('vue');
+
+module.exports = Vue.directive('superplaceholder', function(placeholders){
+    var input = this.el;
+    var self = this;
+    self.timeouts = [];
+    self.originalPlaceholder = input.placeholder;
+
+    input.addEventListener('focus', function(){
+        if(placeholders instanceof Array){
+            type(placeholders[0], 0, placeholders, 0)
+        } else {
+            type(placeholders, 0)
+        }
+    })
+
+    input.addEventListener('blur', function(){
+        self.timeouts.forEach(function(timeout){
+            clearTimeout(timeout)
+        })
+        self.timeouts.lenght = 0;
+        input.placeholder = self.originalPlaceholder;
+    })
+
+    function type(word, position, listOfWords, indexOfWord){
+        var newTimeout = setTimeout(function(){
+            if(position <= word.length){
+                input.placeholder = word.substr(0, position) + (position === word.length?'':'|');
+                position++;
+                type(word, position, listOfWords, indexOfWord);
+            } else if( listOfWords && indexOfWord < listOfWords.length){
+                indexOfWord++;
+                setTimeout(function(){
+                    type(listOfWords[indexOfWord], 0, listOfWords, indexOfWord);
+                }, 400)
+            }
+        }, 100)
+        self.timeouts.push(newTimeout);
+    }
+})
